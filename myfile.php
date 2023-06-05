a8df77d97fa774ca79384938a0a54c3afc3d1b91
Okikeonyi <133234771+Okikeonyi@users.noreply.github.com>
2023-06-05 13:16:43 +0100

    Create myfile.php

---
+++ myfile.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Data</title>
+</head>
+<body>
+
+<?php
+// Student Data
+$name = "OKIKE ONYINYECHI ANTHONIA";
+$registrationNumber = "2021/HND/33130/CS";
+$stateOfOrigin = "Ebonyi State";
+$projectTopic = " Design and Implementation of Crime Documentation System";
+
+// Output the data 
+echo "<h1>Student Data</h1>";
+echo "<p><strong>Name:</strong> " . $name . "</p>";
+echo "<p><strong>Registration Number:</strong> " . $registrationNumber . "</p>";
+echo "<p><strong>State of Origin:</strong> " . $stateOfOrigin . "</p>";
+echo "<p><strong>Project Topic:</strong> " . $projectTopic . "</p>";
+?>
+
+</body>
+</html>
